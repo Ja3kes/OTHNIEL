3b4f44062c4e3d62ad146c5a6fd39845a05984bc
Ja3kes <jakeskeshi100@gmail.com>
2025-09-27 17:26:33 +0100

    initial commit

---
+++ arrowfunctions.js
@@ -0,0 +1,7 @@
+
+const multiply=(a,b)=>{
+  console.log(a*b);
+}
+multiply(4,5);
+multiply(6,7);
+multiply(8,9);
---
+++ ternary.js
@@ -0,0 +1,4 @@
+
+let age=5;
+const message=age>=16 ? "you are an adult" :"you are child"
+console.log(message);
